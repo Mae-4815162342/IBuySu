4aece328ba709d585b3e9e6751825ab063bcab80
Speykious <speykious@gmail.com>
2021-12-14 18:04:32 +0100

    Javadoc-umenter la classe `Acheteur`

--- System/Adresse.java
+++ System/Adresse.java
@@ -1,5 +1,8 @@
 package System;
 
+/**
+ * Une adresse géographique.
+ */
 public class Adresse {
     private String ville;
     private int codePostal;
@@ -7,6 +10,15 @@ public class Adresse {
     private int numeroRue;
     private String nomRue;
 
+    /**
+     * Crée une Adresse.
+     * 
+     * @param numRue Numéro de la rue.
+     * @param nomRue Nom de la rue.
+     * @param codePostal Code postal.
+     * @param ville Ville.
+     * @param pays Pays.
+     */
     public Adresse(int numRue, String nomRue, int codePostal, String ville, String pays) {
         this.numeroRue = numRue;
         this.nomRue = nomRue;
@@ -15,13 +27,16 @@ public class Adresse {
         this.pays = pays;
     }
 
-    // cette fonction vérifie simplement que le code postal ne dépasse pas 99 999
-    // afin que le format ne soit pas
-    // absurde
+    /**
+     * Vérifie que le format du code postal est valide.
+     * @param codePostal
+     * @return
+     */
     public boolean verif(int codePostal) {
         return codePostal >= 0 && codePostal < 100_000;
     }
 
+    @Override
     public String toString() {
         return numeroRue + " " + nomRue + ", " + codePostal + " " + ville + ", " + pays;
     }
