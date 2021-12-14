fbc1ecb4ed688362e6f03725c7b99527cedac544
Speykious <speykious@gmail.com>
2021-12-14 19:59:10 +0100

    J'AI FINI

--- System/Inscrit.java
+++ System/Inscrit.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import BDD.API;
 
+/**
+ * Utilisateur possédant un compte dans la base de donnée de iBuySu.com.
+ */
 public abstract class Inscrit extends Utilisateur {
     protected String nom;
     protected String prenom;
@@ -16,6 +19,20 @@ public abstract class Inscrit extends Utilisateur {
     protected List<Evaluation> evaluations = new ArrayList<Evaluation>();
     protected ArrayList<Contrat> contrats = new ArrayList<Contrat>();
 
+    /**
+     * Crée un utilisateur inscrit.
+     * @param pseudo Pseudo de l'utilisateur inscrit.
+     * @param nom Nom de l'utilisateur inscrit.
+     * @param prenom Prénom de l'utilisateur inscrit.
+     * @param numTel Numéro de téléphone de l'utilisateur inscrit.
+     * @param mail Adresse mail de l'utilisateur inscrit.
+     * @param mdp Mot de passe de l'utilisateur inscrit.
+     * @param numRue Numéro de rue de l'utilisateur inscrit.
+     * @param nomRue Nom de rue de l'utilisateur inscrit.
+     * @param codePostal Code postal de l'utilisateur inscrit.
+     * @param ville Ville de l'utilisateur inscrit.
+     * @param pays Pays de l'utilisateur inscrit.
+     */
     public Inscrit(String pseudo, String nom, String prenom, int numTel, String mail, String mdp, int numRue,
             String nomRue, int codePostal, String ville, String pays) {
         this.pseudo = pseudo;
@@ -28,6 +45,21 @@ public abstract class Inscrit extends Utilisateur {
         this.motdepasse = mdp;
     }
 
+    /**
+     * Crée un utilisateur inscrit.
+     * @param id ID de l'utilisateur inscrit.
+     * @param pseudo Pseudo de l'utilisateur inscrit.
+     * @param nom Nom de l'utilisateur inscrit.
+     * @param prenom Prénom de l'utilisateur inscrit.
+     * @param numTel Numéro de téléphone de l'utilisateur inscrit.
+     * @param mail Adresse mail de l'utilisateur inscrit.
+     * @param mdp Mot de passe de l'utilisateur inscrit.
+     * @param numRue Numéro de rue de l'utilisateur inscrit.
+     * @param nomRue Nom de rue de l'utilisateur inscrit.
+     * @param codePostal Code postal de l'utilisateur inscrit.
+     * @param ville Ville de l'utilisateur inscrit.
+     * @param pays Pays de l'utilisateur inscrit.
+     */
     public Inscrit(int id, String pseudo, String nom, String prenom, int numTel, String mail, String mdp, int numRue,
                    String nomRue, int codePostal, String ville, String pays) {
         this.pseudo = pseudo;
@@ -40,19 +72,34 @@ public abstract class Inscrit extends Utilisateur {
         this.motdepasse = mdp;
     }
 
+    /**
+     * Vérifie que le mot de passe est bien celui de l'utilisateur inscrit.
+     * @param mdp Mot de passe à vérifier.
+     * @return si le mot de passe est bien celui de l'utilisateur inscrit.
+     */
     public boolean verifMdp(String mdp) {
         return mdp.equals(motdepasse);
     }
 
+    /**
+     * Récupère le formulaire permettant à l'utilisateur de se connecter.
+     */
     public static String[] getFormulaireConnexion() {
         String[] res = {"mail", "mot de passe"};
         return res;
     }
 
+    /**
+     * Ajoute un contrat.
+     * @param c Contrat à ajouter.
+     */
     public void addContrat(Contrat c) {
         contrats.add(c);
     }
 
+    /**
+     * Récupère tous les contrats qui ont été conclus.
+     */
     public ArrayList<Contrat> getClosedContracts() {
         ArrayList<Contrat> res = new ArrayList<Contrat>();
         for (Contrat contrat : contrats) {
@@ -63,72 +110,111 @@ public abstract class Inscrit extends Utilisateur {
         return res;
     }
 
+    /**
+     * Permet à l'utilisateur inscrit de créer une évaluation.
+     * @param titreProduit Titre du produit à évaluer.
+     * @param note Note de l'évaluation.
+     * @param description Description de l'évaluation.
+     * @param destinataire Destinataire de l'évaluation.
+     */
     public void creerEvaluation(String titreProduit, int note, String description, Inscrit destinataire) {
         Evaluation evaluation = new Evaluation(titreProduit, note, description, this, destinataire);
         this.evaluations.add(evaluation);
     }
 
+    /**
+     * Récupère le menu d'un utilisateur inscrit.
+     */
     public String[] getMenu() {
         String[] menu = {"Recherche", "Déconnexion", "Quitter"};
         return menu;
     }
 
+    /**
+     * @return Les données publiques de l'utilisateur, c'est-à-dire son identifiant et son pseudo.
+     */
     @Override
-    // cette méthode retourne les données publiques de l'utilisateur, c'est-à-dire
-    // son identifiant et son pseudonyme
     public String getAffichageMinimal() {
         return pseudo + " (id:" + id + ")";
     }
 
-    // affichage complet, qui ne peut être vu que par l'inscrit lui-même (les
-    // coordonnées partagées lors des ventes
-    // sont gérées dans les sous-classes Acheteur et Vendeur)
-    // !!! le mot de passe n'est JAMAIS affiché
+    /**
+     * Affichage complet, qui ne peut être vu que par l'inscrit lui-même (les coordonnées partagées lors des ventes sont gérées dans les sous-classes Acheteur et Vendeur)
+     * !!! Le mot de passe n'est JAMAIS affiché !!!
+     */
+    @Override
     public String toString() {
         return this.getAffichageMinimal() + '\n' + nom + ' ' + prenom + '\n' + mail + ' ' + numeroTel + '\n'
                 + adresse.toString();
     }
 
+    /**
+     * ID de l'utilisateur inscrit.
+     */
     public int getId() {
         // Automatically generated method. Please do not modify this code.
         return this.id;
     }
 
+    /**
+     * Nom de l'utilisateur inscrit.
+     */
     public String getNom() {
         // Automatically generated method. Please do not modify this code.
         return this.nom;
     }
 
+    /**
+     * Prénom de l'utilisateur inscrit.
+     */
     public String getPrenom() {
         // Automatically generated method. Please do not modify this code.
         return this.prenom;
     }
 
+    /**
+     * Numéro de téléphone de l'utilisateur inscrit.
+     */
     public int getNumeroTel() {
         // Automatically generated method. Please do not modify this code.
         return this.numeroTel;
     }
 
+    /**
+     * Pseudo de l'utilisateur inscrit.
+     */
     public String getPseudo() {
         // Automatically generated method. Please do not modify this code.
         return this.pseudo;
     }
 
+    /**
+     * Adresse mail de l'utilisateur inscrit.
+     */
     public String getMail() {
         // Automatically generated method. Please do not modify this code.
         return this.mail;
     }
 
+    /**
+     * Mot de passe de l'utilisateur inscrit.
+     */
     public String getMotdepasse() {
         // Automatically generated method. Please do not modify this code.
         return this.motdepasse;
     }
 
+    /**
+     * Adresse de l'utilisateur inscrit.
+     */
     public Adresse getAdresse() {
         // Automatically generated method. Please do not modify this code.
         return this.adresse;
     }
 
+    /**
+     * Formulaire permettant à un utilisateur de s'inscrire sur iBuySu.com.
+     */
     public static String[] getFormulaireInscription() {
         String[] res = { "pseudo", "nom", "prénom", "numéro de teléphone", "mail", "mot de passe", "n°rue", "nom rue",
                 "code postal", "ville", "pays" };
