e2afc3df2b44ba6add9d5b07b8898c27f0d4170b
Speykious <speykious@gmail.com>
2021-12-14 19:41:41 +0100

    Javadoc-umenter la classe `Produit`

--- System/Produit.java
+++ System/Produit.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import BDD.*;
 
+/**
+ * Produit. À ne pas confondre avec Utilisateur.
+ */
 public class Produit {
     protected int id;
     protected static int nbProd = API.fetchNbProduct();
@@ -18,8 +21,18 @@ public class Produit {
     protected boolean isReceived;
     protected Contrat contrat = null;
 
+    /**
+     * Crée un produit.
+     * 
+     * @param titre Titre du produit.
+     * @param desc  Description du produit.
+     * @param v     Vendeur du produit.
+     * @param photo Photo du produit.
+     * @param prix  Prix du produit.
+     * @param c     Catégorie du produit.
+     */
     public Produit(String titre, String desc, Vendeur v, String photo, float prix, Categorie c) {
-        nbProd ++;
+        nbProd++;
         this.id = nbProd;
         this.titre = titre;
         this.description = desc;
@@ -31,6 +44,17 @@ public class Produit {
         this.isReceived = false;
     }
 
+    /**
+     * Crée un produit.
+     * 
+     * @param id    ID du produit.
+     * @param titre Titre du produit.
+     * @param desc  Description du produit.
+     * @param v     Vendeur du produit.
+     * @param photo Photo du produit.
+     * @param prix  Prix du produit.
+     * @param c     Catégorie du produit.
+     */
     public Produit(int id, String titre, String desc, Vendeur v, String photo, float prix, Categorie c) {
         this.id = id;
         this.titre = titre;
@@ -43,18 +67,32 @@ public class Produit {
         this.isReceived = false;
     }
 
-    public Produit(String[] formulaireRempli, Vendeur v, Categorie c){
-        this(formulaireRempli[0], formulaireRempli[1], v, formulaireRempli[2], Float.parseFloat(formulaireRempli[3]), c);
+    /**
+     * Crée un produit à partir d'un formulaire prérempli par l'utilisateur.
+     * 
+     * @param formulaireRempli Formulaire prérempli par l'utilisateur.
+     * @param v                Vendeur du produit.
+     * @param c                Catégorie du produit.
+     */
+    public Produit(String[] formulaireRempli, Vendeur v, Categorie c) {
+        this(formulaireRempli[0], formulaireRempli[1], v, formulaireRempli[2], Float.parseFloat(formulaireRempli[3]),
+                c);
     }
 
+    /**
+     * Ajoute un mot-clef pour le produit.
+     * 
+     * @param m Mot-clef.
+     */
     public void addMotClef(MotClef m) {
         motClefs.add(m);
     }
 
-
+    @Override
     public String toString() {
         String res = this.titre + "\n" + this.description + '\n' + this.photo + '\n';
-        res += "Vendu par " + ((this.vendeur != null) ? this.vendeur.getAffichageMinimal() : "anonyme") + " a " + this.prix + "€\n";
+        res += "Vendu par " + ((this.vendeur != null) ? this.vendeur.getAffichageMinimal() : "anonyme") + " a "
+                + this.prix + "€\n";
         res += "Categorie : " + categorie.getNom() + "\n" + "Mot-clefs : ";
         if (motClefs.size() == 0) {
             res += "Aucun mot-clef";
@@ -66,81 +104,137 @@ public class Produit {
         return res;
     }
 
+    /**
+     * Titre du produit.
+     */
     public String getTitre() {
         // Automatically generated method. Please do not modify this code.
         return this.titre;
     }
 
+    /**
+     * Description du produit.
+     */
     public String getDescription() {
         // Automatically generated method. Please do not modify this code.
         return this.description;
     }
 
+    /**
+     * Détermine la description du produit.
+     * 
+     * @param value
+     */
     public void setDescription(final String value) {
         // Automatically generated method. Please do not modify this code.
         this.description = value;
     }
 
+    /**
+     * Photo du produit.
+     * 
+     * @return
+     */
     public String getPhoto() {
         // Automatically generated method. Please do not modify this code.
         return this.photo;
     }
 
+    /**
+     * Détermine la photo du produit.
+     */
     public void setPhoto(final String value) {
         // Automatically generated method. Please do not modify this code.
         this.photo = value;
     }
 
+    /**
+     * Prix de départ du produit.
+     */
     public float getPrix_de_depart() {
         // Automatically generated method. Please do not modify this code.
         return this.prix;
     }
 
+    /**
+     * Détermine le prix de départ du produit.
+     */
     public void setPrix_de_depart(final int value) {
         // Automatically generated method. Please do not modify this code.
         this.prix = value;
     }
 
+    /**
+     * Conclut la vente du produit avec un acheteur.
+     * 
+     * @param acheteur Utilisateur ayant acheté le produit.
+     * @return Le contrat de vente de la transaction.
+     */
     public Contrat conclureVente(Acheteur acheteur) {
         this.contrat = new Contrat(acheteur, this.vendeur, this, this.prix);
         this.isSold = true;
         return contrat;
     }
 
-    //TODO : Ajout de la méthode refuserVente()
+    /**
+     * Permet au vendeur de refuser la vente.
+     * 
+     * @return le contrat précédent.
+     */
     public Contrat refuserVente() {
         Contrat previousContract = this.contrat;
         this.contrat = null;
         return previousContract;
     }
 
+    /**
+     * Détermine le contrat du produit.
+     */
     public void setContrat(Contrat contrat) {
         // Automatically generated method. Please do not modify this code.
         this.contrat = contrat;
     }
 
-
+    /**
+     * Récupère le formulaire permettant de remplir les informations d'un produit.
+     */
     public static String[] getFormulaire() {
-        String[] res = {"titre", "description", "photo", "prix"};
+        String[] res = { "titre", "description", "photo", "prix" };
         return res;
     }
 
-    public Vendeur getVendeur(){return vendeur;}
+    /**
+     * Récupère le vendeur.
+     */
+    public Vendeur getVendeur() {
+        return vendeur;
+    }
 
+    /**
+     * Si le produit est vendu.
+     */
     public boolean getEstVendu() {
         return isSold;
     }
 
+    /**
+     * Si le produit a été reçu.
+     */
     public boolean getEstRecu() {
         return isReceived;
     }
 
+    /**
+     * Catégorie du produit.
+     */
     public Categorie getCategorie() {
         return categorie;
     }
 
+    /**
+     * ID du produit.
+     */
     public int getId() {
         return id;
     }
-
 }
