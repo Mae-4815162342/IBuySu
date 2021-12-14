b860fef17b4c796b12cb6c106d1b5d831e1bd0de
Speykious <speykious@gmail.com>
2021-12-14 18:31:40 +0100

    Javadoc-umenter la classe `DonneesBancaires`

--- System/DonneesBancaires.java
+++ System/DonneesBancaires.java
@@ -1,9 +1,16 @@
 package System;
 
+/**
+ * Données bancaires.
+ */
 public abstract class DonneesBancaires {
     protected String banque;
     protected String titulaire;
 
+    /**
+     * Formulaire permettant à l'utilisateur de saisir ses données bancaires.
+     * @param type Type de données bancaires. Soit "RIB" pour un compte, soit "CB" pour une carte.
+     */
     public static String[] getFormulaire(String type) {
         switch (type) {
             case "RIB":
@@ -16,10 +23,17 @@ public abstract class DonneesBancaires {
         return null;
     }
 
+    /**
+     * Vérifie les données bancaires du vendeur.
+     * @param formulaireVendeur Formulaire de vendeur.
+     * @param formulaireBanque Formulaire de données bancaires.
+     * @return true si les données bancaires du vendeur sont valides, false sinon.
+     */
     public static boolean verifierVendeur(String[] formulaireVendeur, String[] formulaireBanque) {
         return formulaireBanque[0] == formulaireVendeur[1] && formulaireBanque[1] == formulaireVendeur[2];
     }
 
+    @Override
     public String toString() {
         return this.titulaire + "(" + this.banque + ')';
     }
